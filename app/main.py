4d822fb1fd191b481e5d7070cbd47d8573e2147f
ellensilva-dev <linoellenlinosilva@gmail.com>
2026-09-21 20:09:44 -0300

    "Review Summary"

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
-        self.health = 100
+        self.health = health
         self.hidden = False
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+    def __str__(self) -> str:
         return (
             f"{{Name: {self.name}, "
             f"Health: {self.health}, "
